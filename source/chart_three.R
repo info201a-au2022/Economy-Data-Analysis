2d90aa9c7027c1b98674c3a0ae23b63395855b22
amyo6 <amyo6@uw.edu>
2022-11-14 15:22:22 -0800

    chart 3 work again

--- source/chart_three.R
+++ source/chart_three.R
@@ -1,10 +1,12 @@
 # source file that creates your third chart
-credit_classification <- read.csv("/Users/amyofor/Documents/info201/project-group-7-section-ab/data/test.csv/test.csv",stringsAsFactors = FALSE)
+credit_classification <- read.csv("../data/credit_class.csv")
 View(credit_classification)
+getwd()
 install.packages("tidyverse")
 library(tidyverse)
 install.packages("ggplot2")
 plot_of_age_vs_annualincome <- ggplot(data = credit_classification) +
-  geom_col(mapping = aes(x = Age, y = Annual_Income))+
+  geom_col(mapping = aes(x = Age, y = Annual_Income, fill = Occupation)) +
   scale_y_continuous(labels = scales::comma)
 plot_of_age_vs_annualincome
+
