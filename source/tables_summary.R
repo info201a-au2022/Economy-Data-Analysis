fe80b97b01fd971e0ffd8adb7a242310f9e1cfcd
amyo6 <amyo6@uw.edu>
2022-11-14 14:56:42 -0800

    table stuff for chart 3

--- source/tables_summary.R
+++ source/tables_summary.R
@@ -1,2 +1,10 @@
 # source file that creates a table of summary information 
 # to be included in your report
+credit_classification <- read.csv("/Users/amyofor/Documents/info201/project-group-7-section-ab/data/credit_class.csv")
+View(credit_classification)
+table <- credit_classification %>% 
+  group_by(Age) %>%
+  summarise(avg_annual_income = mean(Annual_Income, na.rm = T)) %>%
+  arrange((avg_annual_income))
+View(table)
+
