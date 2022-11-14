fd3935414df0fb25c638390247df5eb8a4404409
Lesther <lestherdumosmog@gmail.com>
2022-11-14 15:55:15 -0800

    few summary tables

--- source/tables_summary.R
+++ source/tables_summary.R
@@ -1,13 +1,16 @@
 # source file that creates a table of summary information 
 # to be included in your report
-credit_classification <- read.csv("../data/credit_class.csv")
-View(credit_classification)
-getwd()
-setwd()
-View(credit_classification)
-table <- credit_classification %>% 
-  group_by(Age) %>%
-  summarise(avg_annual_income = mean(Annual_Income, na.rm = T)) %>%
-  arrange((avg_annual_income))
-View(table)
+
+# Files
+credit_classification <- read.csv("../data/credit_class.csv", stringsAsFactors = FALSE)
+house_price <- read.csv("../data/house_prices.csv", stringsAsFactors = FALSE)
+
+#Table Summaries
+house_table_summary <- house_price %>% 
+  group_by(FirstQuarterYearly) %>% 
+  select(AverageSalesPricesOfHousesInTheUS)
+
+credit_table_summary <- credit_classification %>% 
+  group_by(Occupation) %>% 
+  select(Annual_Income)
 
