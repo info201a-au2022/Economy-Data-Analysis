217f909b78ec503a51a33d189efe2daecfa76fff
amyo6 <amyo6@uw.edu>
2022-11-14 18:29:07 -0800

    one more change to chart 3

--- source/chart_three.R
+++ source/chart_three.R
@@ -1,6 +1,8 @@
 credit_classification <- read_csv("../data/credit_class.csv")
 View(credit_classification)
+library(ggplot2)
 plot_of_age_vs_occupation <- ggplot(data = credit_classification) +
    geom_col(mapping = aes(x = Age, y = Occupation, fill = Annual_Income)) +
-  xlim(0,100)
+  xlim(0,100) +
+  ggtitle("Annual Income in Relation to Age and Occupation")
 plot_of_age_vs_occupation
