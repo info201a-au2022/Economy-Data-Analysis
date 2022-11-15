c229454f71af6be1f6af18e63d816ef7b21ef153
Lesther <lestherdumosmog@gmail.com>
2022-11-15 08:50:27 -0800

    My commits are not going thru?

--- source/summary_information.R
+++ source/summary_information.R
@@ -9,13 +9,14 @@ credit_class <- read.csv("../data/credit_class.csv", stringsAsFactors = FALSE)
 
 source("tables_summary.R")
 
-sum_percent_income <- function() {
-  change_1 <- avg_income %>% 
-    sum(PercentChange, na.rm = TRUE) %>% 
-    pull(change_1)
-  return(change_1)
+sum_percent_income <- sum(avg_income$PercentChange, na.rm = TRUE)
+
+sum_percent_income_v2 <- function () {
+  percent_change <- avg_income %>% 
+    summarize(total_change = sum(PercentChange, na.rm=TRUE)) %>% 
+    pull(total_change)
+  return(percent_change)
 }
-sum(avg_income$PercentChange, na.rm = TRUE)
 
 sum_percent_house <- sum(house_prices$PercentChange, na.rm = TRUE)
 
