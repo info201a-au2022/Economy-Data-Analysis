7907181f8efe75db1cf03122cee0c4d9d74220da
amyo6 <amyo6@uw.edu>
2022-11-14 18:02:17 -0800

    chart with correct values

--- source/chart_three.R
+++ source/chart_three.R
@@ -5,8 +5,12 @@ getwd()
 install.packages("tidyverse")
 library(tidyverse)
 install.packages("ggplot2")
-plot_of_age_vs_annualincome <- ggplot(data = credit_classification) +
-  geom_col(mapping = aes(x = Age, y = Annual_Income, fill = Occupation)) +
-  scale_y_continuous(labels = scales::comma)
-plot_of_age_vs_annualincome
+annual_income <- credit_classification$Annual_Income
+max_annual_income <- max(annual_income)
+print(max_annual_income)
+view(annual_income)
+plot_of_age_vs_occupation <- ggplot(data = credit_classification) +
+  geom_col(mapping = aes(x = Age, y = Occupation, fill = Annual_Income)) +
+  xlim(0,100)
+plot_of_age_vs_occupation
 
